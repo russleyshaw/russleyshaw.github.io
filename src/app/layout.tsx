14b167ef68b3807da0a008474947294cdf421f68
Russley Shaw <russleyshaw@gmail.com>
2024-06-15 07:28:19 -0500

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ export const metadata = {
     description: "Russley's personal site",
 };
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Link from "next/link";
 import "../styles/global.css";
 
@@ -12,6 +14,7 @@ export default function RootLayout(props: {
     return (
         <html lang="en">
             <body className="p-4 flex flex-col gap-4">
+                <SpeedInsights />
                 <div className="flex flex-row items-center gap-4">
                     <h1>Russley</h1>
                     <Link href="/blog">Blog</Link>
