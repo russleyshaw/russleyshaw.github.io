c686c1e1b53c11734e7819cd7155e6337b0094dd
Russley Shaw <russleyshaw@gmail.com>
2024-11-02 21:53:21 -0500

    Reset site

--- src/components/Muted.tsx
+++
@@ -1,3 +0,0 @@
-export default function Muted(props: { children: React.ReactNode }) {
-    return <span className="opacity-50 text-xs">{props.children}</span>;
-}
--- src/components/Tag.tsx
+++
@@ -1,7 +0,0 @@
-interface TagProps {
-    children?: React.ReactNode;
-}
-
-export default function Tag(props: TagProps) {
-    return <div className="text-xs p-1 rounded border border-white">{props.children}</div>;
-}
