d6e2d2536dfeb236871780f42a5dc08562289112
Russley Shaw <russleyshaw@gmail.com>
2024-11-03 21:53:31 -0600

    Add back vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,12 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://russley.dev",
-  integrations: [react(), tailwind(), mdx(), sitemap()]
+  integrations: [react(), tailwind(), mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel()
 });
